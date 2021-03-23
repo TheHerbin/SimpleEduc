71731c11a161a2044aa6f9dedc9031cb7aa468f9
Therbin <theo.herbin59710@outlook.fr>
2021-03-23 07:44:39 +0000

    new DB

--- localhost.sql
+++ localhost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 09 mars 2021 à 15:46
+-- Généré le : mar. 23 mars 2021 à 07:43
 -- Version du serveur :  10.3.27-MariaDB-0+deb10u1
 -- Version de PHP : 7.3.27-1~deb10u1
 
@@ -27,6 +27,44 @@ USE `simpleeduc`;
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `cahierDesCharges`
+--
+
+CREATE TABLE `cahierDesCharges` (
+  `id` int(11) NOT NULL,
+  `idEntreprise` int(11) NOT NULL,
+  `etat` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(250) NOT NULL,
+  `prenom` varchar(250) NOT NULL,
+  `tel` varchar(10) NOT NULL,
+  `adresse` varchar(250) NOT NULL,
+  `idEntreprise` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contrat`
+--
+
+CREATE TABLE `contrat` (
+  `id` int(11) NOT NULL,
+  `idClient` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `developpeurs`
 --
 
@@ -45,9 +83,103 @@ CREATE TABLE `developpeurs` (
 INSERT INTO `developpeurs` (`id`, `nom`, `prenom`, `indiceremuneration`, `couthoraire`) VALUES
 (2, 'Dupont', 'Richard', 1.2, 8.33);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Entreprise`
+--
+
+CREATE TABLE `Entreprise` (
+  `id` int(11) NOT NULL,
+  `adresse` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `tel` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `equipeDev`
+--
+
+CREATE TABLE `equipeDev` (
+  `id` int(11) NOT NULL,
+  `idResponsable` int(11) NOT NULL,
+  `idProjet` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `outil`
+--
+
+CREATE TABLE `outil` (
+  `code` int(11) NOT NULL,
+  `libelle` varchar(250) NOT NULL,
+  `version` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `outil_developpeurs`
+--
+
+CREATE TABLE `outil_developpeurs` (
+  `idDev` int(11) NOT NULL,
+  `idOutil` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `projet`
+--
+
+CREATE TABLE `projet` (
+  `id` int(10) NOT NULL,
+  `libelle` int(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tache`
+--
+
+CREATE TABLE `tache` (
+  `id` int(11) NOT NULL,
+  `libelle` varchar(250) NOT NULL,
+  `idDev` int(11) NOT NULL,
+  `heuresPassees` int(15) NOT NULL,
+  `idProjet` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `cahierDesCharges`
+--
+ALTER TABLE `cahierDesCharges`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idEntreprise_2` (`idEntreprise`);
+
+--
+-- Index pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idEntreprise` (`idEntreprise`);
+
+--
+-- Index pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idClient` (`idClient`);
 
 --
 -- Index pour la table `developpeurs`
@@ -56,14 +188,145 @@ ALTER TABLE `developpeurs`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `Entreprise`
+--
+ALTER TABLE `Entreprise`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `equipeDev`
+--
+ALTER TABLE `equipeDev`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idResponsable` (`idResponsable`),
+  ADD KEY `idProjet` (`idProjet`);
+
+--
+-- Index pour la table `outil`
+--
+ALTER TABLE `outil`
+  ADD PRIMARY KEY (`code`);
+
+--
+-- Index pour la table `outil_developpeurs`
+--
+ALTER TABLE `outil_developpeurs`
+  ADD KEY `idoutil` (`idOutil`),
+  ADD KEY `idDEv` (`idDev`);
+
+--
+-- Index pour la table `projet`
+--
+ALTER TABLE `projet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idDev` (`idDev`),
+  ADD KEY `idProjet` (`idProjet`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `cahierDesCharges`
+--
+ALTER TABLE `cahierDesCharges`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `developpeurs`
 --
 ALTER TABLE `developpeurs`
   MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `Entreprise`
+--
+ALTER TABLE `Entreprise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `equipeDev`
+--
+ALTER TABLE `equipeDev`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `outil`
+--
+ALTER TABLE `outil`
+  MODIFY `code` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `projet`
+--
+ALTER TABLE `projet`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `tache`
+--
+ALTER TABLE `tache`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `cahierDesCharges`
+--
+ALTER TABLE `cahierDesCharges`
+  ADD CONSTRAINT `cahierDesCharges_ibfk_1` FOREIGN KEY (`idEntreprise`) REFERENCES `Entreprise` (`id`);
+
+--
+-- Contraintes pour la table `contact`
+--
+ALTER TABLE `contact`
+  ADD CONSTRAINT `idEntreprise` FOREIGN KEY (`idEntreprise`) REFERENCES `Entreprise` (`id`);
+
+--
+-- Contraintes pour la table `contrat`
+--
+ALTER TABLE `contrat`
+  ADD CONSTRAINT `idClient` FOREIGN KEY (`idClient`) REFERENCES `Entreprise` (`id`);
+
+--
+-- Contraintes pour la table `equipeDev`
+--
+ALTER TABLE `equipeDev`
+  ADD CONSTRAINT `idProjet` FOREIGN KEY (`idProjet`) REFERENCES `projet` (`id`),
+  ADD CONSTRAINT `idResponsable` FOREIGN KEY (`idResponsable`) REFERENCES `developpeurs` (`id`);
+
+--
+-- Contraintes pour la table `outil_developpeurs`
+--
+ALTER TABLE `outil_developpeurs`
+  ADD CONSTRAINT `idDEv` FOREIGN KEY (`idDev`) REFERENCES `developpeurs` (`id`),
+  ADD CONSTRAINT `idoutil` FOREIGN KEY (`idOutil`) REFERENCES `outil` (`code`);
+
+--
+-- Contraintes pour la table `tache`
+--
+ALTER TABLE `tache`
+  ADD CONSTRAINT `tache_ibfk_1` FOREIGN KEY (`idDev`) REFERENCES `developpeurs` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
